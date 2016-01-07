353d5c7a9f39d6bcb4c73df787d19f710b6f2c42
jotauses <jota.us.es@gmail.com>
2016-01-07 20:25:09 +0100

    Subido proyecto
    Subido proyecto

---
+++ src/main/java/PruebaApp/App.java
@@ -0,0 +1,13 @@
+package PruebaApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
